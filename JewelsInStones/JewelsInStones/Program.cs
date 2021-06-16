7126aec194b478a0b705f7c7eef2077ddab99dad
MACIEJ CZERWINSKI <MACIEJ.CZERWINSKI@BEAUTYOFSCIENCE.PL>
2021-06-16 14:18:00 +0200

    JewelsInStones_v1

---
+++ JewelsInStones/JewelsInStones/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace JewelsInStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Comparison c = new Comparison();
+            Console.WriteLine(c.CountJewelInStones("z", "ZZ"));
+            Console.WriteLine(c.CountJewelInStones("aA", "aAAbbbb"));
+            Console.WriteLine(c.CountJewelsInStonesLinq("z", "ZZ"));
+            Console.WriteLine(c.CountJewelsInStonesLinq("aA", "aAAbbbb"));
+        }
+
+        public class Comparison
+        {
+            public int CountJewelInStones(string jewels, string stones)
+            {
+                int result = 0;
+                foreach (var item in stones)
+                {
+                    foreach (var el in jewels)
+                    {
+                        if (item == el)
+                            result++;
+                    }
+                }
+                return result;
+            }
+
+            public int CountJewelsInStonesLinq(string jewels, string stones)
+            {
+                return stones.Where(x => jewels.Contains(x)).Count();
+            }
+        }
+    }
+}
